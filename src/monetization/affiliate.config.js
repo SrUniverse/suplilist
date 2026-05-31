adcf4ed88f6d0def44797e9ca2fea85159a0260d
SrUniverse <sruniverse@github.com>
2026-05-31 17:42:53 -0300

    chore(monetization): add TODO comments for placeholder affiliate IDs

--- src/monetization/affiliate.config.js
+++ src/monetization/affiliate.config.js
@@ -1,5 +1,5 @@
 export const AFFILIATE_CONFIG = {
   amazon:       'suplilist01-20',
-  mercadolivre: 'SUPLILIST_ML',
-  shopee:       'SUPLILIST_SHOPEE',
+  mercadolivre: 'SUPLILIST_ML',     // TODO: replace with real Mercado Livre partner ID
+  shopee:       'SUPLILIST_SHOPEE', // TODO: replace with real Shopee affiliate ID
 };
